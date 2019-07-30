909b762511293ea63b6f984a475c2aa5b1302991
Andrii Ryzhyi <andrii.nrj@gmail.com>
2019-07-30 19:48:54 +0300

    Point neg

--- lesson_2/point.py
+++ lesson_2/point.py
@@ -23,21 +23,25 @@ class Point:
         self._z = z
 
     def __add__(self, other):
-        return (self._x + other.get_x(), self._y + other.get_y(), self._z + other.get_z())
+        return Point(self._x + other.get_x(), self._y + other.get_y(), self._z + other.get_z())
 
     def __sub__(self, other):
-        return (self._x - other.get_x(), self.get_y() - other.get_y(), self._z - other.get_z())
+        return Point(self._x - other.get_x(), self.get_y() - other.get_y(), self._z - other.get_z())
 
     def __mul__(self, other):
         return Point(self._x * other.get_x(), self._y * other.get_y(), self._z * other.get_z())
 
     def __div__(self, other):
-       return (self._x / other.get_x(), self._y / other.get_y(), self._z / other.get_z())
+       return Point(self._x / other.get_x(), self._y / other.get_y(), self._z / other.get_z())
 
     def __repr__(self):
         return "x = {}, y = {}, z = {}".format(self._x, self._y, self._z)
+
+    def __neg__(self):
+        return Point(self * Point(-1, -1, -1))
 
 p1 = Point(1, 2, 3)
 p2 = Point(1, 2, 3)
 
 print(p1 * p2)
+print(-p1)
